a5a5331c2715a55306bbf1c47cde0c4f02b1ad98
wishtony <vishnutoanother@gmail.com>
2024-01-07 14:45:06 +0530

    commit style

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -6,77 +6,120 @@ import {
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom';
-import "../../styles/Sidebars.css"
-import yourImg from "../../assets/pexels-simon-robben-614810.jpg"
+} from "cdbreact";
+import { NavLink } from "react-router-dom";
+import "../../styles/Sidebars.css";
+import yourImg from "../../assets/pexels-simon-robben-614810.jpg";
 
 const Sidebar = () => {
   return (
-    <div className='d-flexx' style={{ display: 'flex', height: '55rem', overflow: 'scroll initial' }}>
+    <div
+      className="d-flexx"
+      style={{ display: "flex", height: "55rem", overflow: "scroll initial" }}
+    >
       <CDBSidebar textColor="#fff" backgroundColor="#FFFFFF">
-      
-        <CDBSidebarHeader  style={{ color: 'black' }} prefix={<i className="fa fa-bars fa-large"></i>}>
-
-        <div className='d-flex flex-column justify-content-center align-content-center sidebar-head'>
-        <img 
-      src={yourImg}  
-      alt="Your Image" 
-      style={{ width: "45%",marginLeft:"4rem", height: "45%", borderRadius: 400 / 2 }}
-    />
-        <a href="/" className="text-decoration-none" style={{ color: 'black' }}>
-          Ram Mohan
-          </a>
-          <NavLink exact to="/" style={{ color: 'black' }} activeClassName="activeClicked">
-           RamMohan2@gmail.com
+        <CDBSidebarHeader
+          style={{ color: "black" }}
+          prefix={<i className="fa fa-bars fa-large"></i>}
+        >
+          <div className="d-flex flex-column justify-content-center align-content-center sidebar-head">
+            <img
+              src={yourImg}
+              alt="Your Image"
+              style={{
+                width: "45%",
+                marginLeft: "4rem",
+                height: "45%",
+                borderRadius: 400 / 2,
+              }}
+            />
+            <a
+              href="/"
+              className="text-decoration-none"
+              style={{ color: "black" }}
+            >
+              Ram Mohan
+            </a>
+            <NavLink
+              exact
+              to="/"
+              style={{ color: "black" }}
+              activeClassName="activeClicked"
+            >
+              RamMohan2@gmail.com
             </NavLink>
           </div>
-         
         </CDBSidebarHeader>
         <CDBSidebarContent className="sidebar-content">
-          <CDBSidebarMenu >
-          <div className='new-one'>
-            <NavLink exact to="/" style={{ color: 'black' }} activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-            </NavLink>
+          <CDBSidebarMenu>
+            <div className="new-one">
+              <NavLink
+                exact
+                to="/"
+                style={{ color: "black" }}
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="columns">
+                  Dashboard
+                </CDBSidebarMenuItem>
+              </NavLink>
             </div>
-            <div className='new-one'>
-            <NavLink exact to="/tables"  style={{ color: 'black' }}  activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Perks</CDBSidebarMenuItem>
-            </NavLink>
+            <div className="new-one">
+              <NavLink
+                exact
+                to="/tables"
+                style={{ color: "black" }}
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="table">Perks</CDBSidebarMenuItem>
+              </NavLink>
             </div>
-            <div className='new-one'>
-            <NavLink exact to="/profile"  style={{ color: 'black' }}  activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Addons</CDBSidebarMenuItem>
-            </NavLink>
+            <div className="new-one">
+              <NavLink
+                exact
+                to="/profile"
+                style={{ color: "black" }}
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="user">Addons</CDBSidebarMenuItem>
+              </NavLink>
             </div>
-            <div className='new-one'>
-            <NavLink exact to="/analytics"  style={{ color: 'black' }}  activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">FOQ</CDBSidebarMenuItem>
-            </NavLink>
+            <div className="new-one">
+              <NavLink
+                exact
+                to="/analytics"
+                style={{ color: "black" }}
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="chart-line">FOQ</CDBSidebarMenuItem>
+              </NavLink>
             </div>
-<div className='new-one'>
-<NavLink exact to="/hero404"  style={{ color: 'black'}}  target="_blank" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="exclamation-circle">Supports</CDBSidebarMenuItem>
-            </NavLink>
-</div>
-           
+            <div className="new-one">
+              <NavLink
+                exact
+                to="/hero404"
+                style={{ color: "black" }}
+                target="_blank"
+                activeClassName="activeClicked"
+              >
+                <CDBSidebarMenuItem icon="exclamation-circle">
+                  Supports
+                </CDBSidebarMenuItem>
+              </NavLink>
+            </div>
           </CDBSidebarMenu>
         </CDBSidebarContent>
 
-        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-          
-          <div 
+        <CDBSidebarFooter style={{ textAlign: "center" }}>
+          <div
             style={{
-              padding: '20px 5px',
-              color:"#1F7BC5",
-              fontWeight:"bold"
-           
+              padding: "20px 5px",
+              color: "#1F7BC5",
+              fontWeight: "bold",
             }}
           >
-          Log Out
+            Log Out
           </div>
-          
         </CDBSidebarFooter>
       </CDBSidebar>
     </div>
